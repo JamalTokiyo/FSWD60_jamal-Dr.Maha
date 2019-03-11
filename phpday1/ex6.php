eade171b429bac5c6c50bf7b1f412e0f8ded5567
JamalAhmed2019 <jamalusb@gmx.at>
2019-03-11 13:19:52 +0100

    exercise fice and six

---
+++ phpday1/ex6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php exercisr five</title>
+</head>
+<body>
+<?php 
+ $associateArray = array(
+"cartoon"=>"mickeymouse" ,
+"animation" => "SuperMatio" ,
+"game" =>"Goku"
+);
+echo "The cartoon name is: ". $associateArray['cartoon'] . "<img src='bitman.jpg'><br />";
+echo "The animation Name is: ". $associateArray['animation'] . "<br />";
+echo "The gamename Name is: ". $associateArray['game'] . "<br />";
+
+ ?>
+</body>
+</html>
