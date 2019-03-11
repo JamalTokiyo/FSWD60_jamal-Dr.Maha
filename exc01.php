8319a7156284a638701464a1463fc91e28d1241d
mahsa <mahmoudmaha@hotmail.com>
2019-03-11 10:44:11 +0100

    add exc01 and 02 and css files

---
+++ exc01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exc01</title>
+	<link rel="stylesheet" type="text/css" href="exc01.css">
+</head>
+<body>
+
+
+
+	<?php
+	$Name = "Maha Mahmoud";
+	 echo "<h1> $Name </h1>";
+	?>
+
+</body>
+</html>
---
+++ exc02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exc02</title>
+	<link rel="stylesheet" type="text/css" href="exc01.css">
+</head>
+<body>
+
+<?php
+$d=date("D");
+if ($d=="Mon")
+echo "Happy Monday!";
+else
+echo "Have a nice day!";
+?>
+
+</body>
+</html>
