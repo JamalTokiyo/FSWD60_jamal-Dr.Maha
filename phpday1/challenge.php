9f6449f17237e89055afee82c7c767df15c9051b
JamalAhmed2019 <jamalusb@gmx.at>
2019-03-11 14:45:17 +0100

    challenge exercise

---
+++ phpday1/challenge.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<div class="container" style="border: 3px solid lightgray; margin-top: 100px;padding-bottom: 30px">
+		<h2 style="background-color: lightgray; color:blue; text-align: center;margin-top: 10px;" > Car Renting Company:</h2>
+ <form>
+  <div class="form-group">
+    <label for="id">UserID:</label>
+    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Enter ID">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">First Name:</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter first Name">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">last Name:</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter last Name">
+    
+  </div>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Gender
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    
+    <a class="dropdown-item" href="#">Male</a>
+    <a class="dropdown-item" href="#">female</a>
+  </div>
+</div>
+
+  
+
+
+<button type="button" class="btn btn-success btn-lg btn-block">Submit</button>
+</form>
+
+  <?php 
+   
+   ?>
+   </div>
+</body>
+</html>
