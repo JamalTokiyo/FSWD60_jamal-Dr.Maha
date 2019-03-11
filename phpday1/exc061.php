2ad704bd0406bd655f06f133c00d336a697d7057
mahsa <mahmoudmaha@hotmail.com>
2019-03-11 13:19:54 +0100

    add exc061

---
+++ phpday1/exc061.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exc 06</title>
+</head>
+<body>
+
+<?php 
+echo "<br>";
+echo "<br>";
+$grades = array( 
+"Jamal" => 90,
+"Maha" => 80,
+"Ahmed" => 100,
+"Pia" => 80);
+
+echo "grade of Jamal is ".$grades['Jamal']. "<br/>"; echo "<br/>";
+echo "grade of Maha is ".$grades['Maha']. "<br/>";
+echo "grade of Ahmed is ".$grades['Ahmed']. "<br/>";
+echo "grade of Pia is ".$grades['Pia']. "<br/>";
+
+
+
+
+
+
+  ?>
+</body>
+</html>
